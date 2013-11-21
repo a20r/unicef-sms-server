a34e7264ee8b14b33122b55cf3e6b74cbb1cc03e
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-11-21 16:01:26 +0000

    Added the restful spec

---
+++ smsserver/url.py
@@ -0,0 +1,10 @@
+
+""" Base url for the API calls """
+BASE = "http://as245.host.cs.st-andrews.ac.uk:8080/"
+
+""" Used to get data that is ready to be sent """
+TO_SEND = BASE_URL + "to_send/"
+
+""" Used to pass a message from the SMS server to the main server """
+PASS_MESSAGE = BASE_URL + "pass/"
+
